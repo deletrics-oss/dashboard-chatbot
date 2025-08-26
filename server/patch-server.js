4cbab12852d0ff0f8caeec9d3b60524f03f564af
deletrics-oss <deletrics@gmail.com>
2025-08-26 01:34:42 -0300

    Update patch-server.js

--- server/patch-server.js
+++ server/patch-server.js
@@ -10,6 +10,10 @@ app.use(bodyParser.json());
 app.use(patchRouter);
 
 const PORT = 4000;
+const path = require("path");
+app.get("/painel.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "painel.html"));
+});
 app.listen(PORT, () => {
   console.log(`Patch server rodando na porta ${PORT}`);
 });
